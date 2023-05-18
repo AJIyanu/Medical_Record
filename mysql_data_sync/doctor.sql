e61a350d281fd5b10fcaf3c82b154854664df910
AJIyanu <aderemijoseph@live.com>
2023-05-18 20:50:20 +0100

    added tablename

--- mysql_data_sync/doctor.sql
+++ mysql_data_sync/doctor.sql
@@ -4,25 +4,26 @@ LIMIT 0, 1000
 
 -- Date: 2023-05-18 20:25
 */
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('03cd3f44-6993-47ec-a676-211032641823',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('10670bf1-2063-4989-a0fe-928cd29354b5',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('12df2892-0ab7-45f4-ab01-69196f4b032c',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('27e3e15f-4c1e-49b8-9584-8eb07bdbf21b','56345',NULL);
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('27e4d460-1e3e-4390-ab07-5f792a96a131',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('2c65a3fd-88cf-4c8c-ba3b-fcfc3762bb6c',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('363dab92-f78e-4f5b-97f1-958831c98f35',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('3d37efcc-796e-4420-b5f0-9484df306c99',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('3eb0147d-fee4-4958-86fd-f9f846f8a214',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('585e3adc-31fe-4708-a9d0-4e309677591d',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('60b4911c-2810-4b34-93ef-9d8a7d6c9ccd',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('6f3eafdd-55f6-41c7-985a-a753cf79cba2',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('77449093-b166-4730-9bdd-65243b28a232',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('7c4dfb4e-c9b6-41b9-acf4-5db8ebd5af2c',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('86b8a4ec-97be-4911-a890-136077dbe053',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('98bd4e3b-4964-4f38-b5f1-9884bd876021',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('aa74765e-bb61-4c05-94dd-20e71f977e57',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('c56ccf37-b013-42cc-a458-16ce23f47502','56345',NULL);
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('d4eb1e91-26c0-44db-b1f9-864dc4fa0680',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('e167ae6a-f544-4608-a5eb-2e9905721ce0',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('f3f33027-594a-446c-a741-8d46d6e7bc30',NULL,'[\"GENOYSK293\"]');
-INSERT INTO `` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('fc253e1a-e56e-48e1-9f58-979ec6c96025',NULL,'[\"GENOYSK293\"]');
+use Medical_Record;
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('03cd3f44-6993-47ec-a676-211032641823',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('10670bf1-2063-4989-a0fe-928cd29354b5',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('12df2892-0ab7-45f4-ab01-69196f4b032c',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('27e3e15f-4c1e-49b8-9584-8eb07bdbf21b','56345',NULL);
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('27e4d460-1e3e-4390-ab07-5f792a96a131',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('2c65a3fd-88cf-4c8c-ba3b-fcfc3762bb6c',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('363dab92-f78e-4f5b-97f1-958831c98f35',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('3d37efcc-796e-4420-b5f0-9484df306c99',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('3eb0147d-fee4-4958-86fd-f9f846f8a214',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('585e3adc-31fe-4708-a9d0-4e309677591d',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('60b4911c-2810-4b34-93ef-9d8a7d6c9ccd',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('6f3eafdd-55f6-41c7-985a-a753cf79cba2',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('77449093-b166-4730-9bdd-65243b28a232',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('7c4dfb4e-c9b6-41b9-acf4-5db8ebd5af2c',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('86b8a4ec-97be-4911-a890-136077dbe053',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('98bd4e3b-4964-4f38-b5f1-9884bd876021',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('aa74765e-bb61-4c05-94dd-20e71f977e57',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('c56ccf37-b013-42cc-a458-16ce23f47502','56345',NULL);
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('d4eb1e91-26c0-44db-b1f9-864dc4fa0680',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('e167ae6a-f544-4608-a5eb-2e9905721ce0',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('f3f33027-594a-446c-a741-8d46d6e7bc30',NULL,'[\"GENOYSK293\"]');
+INSERT INTO `doctor` (`allperson_id`,`_Doctor__specialization`,`_Doctor__authinst`) VALUES ('fc253e1a-e56e-48e1-9f58-979ec6c96025',NULL,'[\"GENOYSK293\"]');
