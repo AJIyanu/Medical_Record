48255f545f8a56f415b97039b73623b9fc99bc0f
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-06-05 16:48:45 +0100

    Pause

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS Medical_Record;
--- CREATE USER IF NOT EXISTS 'Medics'@'localhost' IDENTIFIED BY 'Medics123';
--- GRANT ALL PRIVILEGES ON `Medical_Record`.* TO 'Medics'@'localhost';
--- GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
--- FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'Medics'@'localhost' IDENTIFIED BY 'Medics123';
+GRANT ALL PRIVILEGES ON `Medical_Record`.* TO 'Medics'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'Medics'@'localhost';
+FLUSH PRIVILEGES;
