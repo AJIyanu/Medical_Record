2319a693ace932910590e6252bedc84a73ec1168
AJIyanu <aderemijoseph@live.com>
2023-06-30 02:31:51 +0100

    sync

--- api/v1/views/vitalsign.py
+++ api/v1/views/vitalsign.py
@@ -11,13 +11,14 @@ from models.permissions import Permissions
 
 
 
-@app_views.route('/lastvitalsign', method=['POST'], strict_slashed=False)
-@jwt_required()
+@app_views.route('/lastvitalsign', methods=['POST'], strict_slashes=False)
+# @jwt_required()
 def last_vital_sign():
     """gets the last vital sign for the patient"""
     details = request.json
-    role = get_jwt().get("role")
-    if not Permissions.me(role).permission("vitalsign", "view"):
-        return jsonify(msg="not permitted"), 403
+#    role = get_jwt().get("role")
+#    print(role)
+#    if not Permissions.me(role).permission("vitalsign", "view"):
+#        return jsonify(msg="not permitted"), 403
     record = VitalSign.pat_last_saved(details.get("patient_id"))
     return jsonify(record=record), 200
