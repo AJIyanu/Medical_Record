0d014028a060a4cf2de357846e229a876fd0838a
AJIyanu <aderemijoseph@live.com>
2023-07-27 19:31:00 +0100

    sync

--- docs/static/scripts/casefile-taginput.js
+++ docs/static/scripts/casefile-taginput.js
@@ -1,112 +1,107 @@
 const tagInputs = document.querySelectorAll('.symp-tag');
-const ul = document.querySelectorAll("ul");
-const ul1 = document.querySelector("ul.allerg");
-const ul2 = document.querySelector("ul.disab");
+const ul = document.querySelectorAll('ul');
 let tags = [];
 let allergy = [];
 let disability = [];
 
-console.log(ul, ul1, ul2);
-
 createTag();
-function createTag(){
-    ul[0].querySelectorAll("li").forEach(li => li.remove());
-    tags.slice().reverse().forEach(tag =>{
-        let liTag = `<li>${tag} <i class="remove-icon" onclick="remove(this, '${tag}')">&times;</i></li>`;
-        ul.insertAdjacentHTML("afterbegin", liTag);
-    });
+function createTag () {
+  ul[0].querySelectorAll('li').forEach(li => li.remove());
+  tags.slice().reverse().forEach(tag => {
+    const liTag = `<li>${tag} <i class="remove-icon" onclick="remove(this, '${tag}')">&times;</i></li>`;
+    ul[0].insertAdjacentHTML('afterbegin', liTag);
+  });
 }
 
-function remove(element, tag){
-    let index  = tags.indexOf(tag);
-    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
-    element.parentElement.remove();
+function remove (element, tag) {
+  const index = tags.indexOf(tag);
+  tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+  element.parentElement.remove();
 }
 
-function addTag(e){
-    const value = e.target.value;
-    if(e.key === ";" || value.indexOf(';') !== -1){
-        e.preventDefault();
-        let tag = e.target.value.replace(/\s+/g, ' ').replace(/;/g, '');
-        if(tag.length > 1 && !tags.includes(tag)){
-            if(tags.length < 20){
-                tag.split(',').forEach(tag => {
-                    tags.push(tag);
-                    createTag();
-                });
-            }
-        }
-        e.target.value = "";
+function addTag (e) {
+  const value = e.target.value;
+  if (e.key === ';' || value.indexOf(';') !== -1) {
+    e.preventDefault();
+    const tag = e.target.value.replace(/\s+/g, ' ').replace(/;/g, '');
+    if (tag.length > 1 && !tags.includes(tag)) {
+      if (tags.length < 20) {
+        tag.split(',').forEach(tag => {
+          tags.push(tag);
+          createTag();
+        });
+      }
     }
+    e.target.value = '';
+  }
 }
 
-tagInputs[0].addEventListener("keyup", addTag);
+tagInputs[0].addEventListener('keyup', addTag);
 
 createAllergy();
-function createAllergy(){
-    ul1.querySelectorAll("li").forEach(li => li.remove());
-    allergy.slice().reverse().forEach(tag =>{
-        let liTag = `<li>${tag} <i class="remove-icon" onclick="removeAllergy(this, '${tag}')">&times;</i></li>`;
-        ul1.insertAdjacentHTML("afterbegin", liTag);
-    });
+function createAllergy () {
+  ul[1].querySelectorAll('li').forEach(li => li.remove());
+  allergy.slice().reverse().forEach(tag => {
+    const liTag = `<li>${tag} <i class="remove-icon" onclick="removeAllergy(this, '${tag}')">&times;</i></li>`;
+    ul[1].insertAdjacentHTML('afterbegin', liTag);
+  });
 }
 
-function removeAllergy(element, tag){
-    let index  = allergy.indexOf(tag);
-    allergy = [...allergy.slice(0, index), ...allergy.slice(index + 1)];
-    element.parentElement.remove();
+function removeAllergy (element, tag) {
+  const index = allergy.indexOf(tag);
+  allergy = [...allergy.slice(0, index), ...allergy.slice(index + 1)];
+  element.parentElement.remove();
 }
 
-function addTag(e){
-    const value = e.target.value;
-    if(e.key === ";" || value.indexOf(';') !== -1){
-        e.preventDefault();
-        let tag = e.target.value.replace(/\s+/g, ' ').replace(/;/g, '');
-        if(tag.length > 1 && !allergy.includes(tag)){
-            if(allergy.length < 20){
-                tag.split(',').forEach(tag => {
-                    allergy.push(tag);
-                    createAllergy();
-                });
-            }
-        }
-        e.target.value = "";
+function allergyAddTag (e) {
+  const value = e.target.value;
+  if (e.key === ';' || value.indexOf(';') !== -1) {
+    e.preventDefault();
+    const tag = e.target.value.replace(/\s+/g, ' ').replace(/;/g, '');
+    if (tag.length > 1 && !allergy.includes(tag)) {
+      if (allergy.length < 20) {
+        tag.split(',').forEach(tag => {
+          allergy.push(tag);
+          createAllergy();
+        });
+      }
     }
+    e.target.value = '';
+  }
 }
 
-tagInputs[1].addEventListener("keyup", addTag);
-
+tagInputs[1].addEventListener('keyup', allergyAddTag);
 
 createDis();
-function createDis(){
-    ul2.querySelectorAll("li").forEach(li => li.remove());
-    disability.slice().reverse().forEach(tag =>{
-        let liTag = `<li>${tag} <i class="remove-icon" onclick="removeDis(this, '${tag}')">&times;</i></li>`;
-        ul2.insertAdjacentHTML("afterbegin", liTag);
-    });
+function createDis () {
+  ul[2].querySelectorAll('li').forEach(li => li.remove());
+  disability.slice().reverse().forEach(tag => {
+    const liTag = `<li>${tag} <i class="remove-icon" onclick="removeDis(this, '${tag}')">&times;</i></li>`;
+    ul[2].insertAdjacentHTML('afterbegin', liTag);
+  });
 }
 
-function removeDis(element, tag){
-    let index  = disability.indexOf(tag);
-    disability = [...disability.slice(0, index), ...disability.slice(index + 1)];
-    element.parentElement.remove();
+function removeDis (element, tag) {
+  const index = disability.indexOf(tag);
+  disability = [...disability.slice(0, index), ...disability.slice(index + 1)];
+  element.parentElement.remove();
 }
 
-function addTag(e){
-    const value = e.target.value;
-    if(e.key === ";" || value.indexOf(';') !== -1){
-        e.preventDefault();
-        let tag = e.target.value.replace(/\s+/g, ' ').replace(/;/g, '');
-        if(tag.length > 1 && !disability.includes(tag)){
-            if(disability.length < 20){
-                tag.split(',').forEach(tag => {
-                    disability.push(tag);
-                    createDis();
-                });
-            }
-        }
-        e.target.value = "";
+function disAddTag (e) {
+  const value = e.target.value;
+  if (e.key === ';' || value.indexOf(';') !== -1) {
+    e.preventDefault();
+    const tag = e.target.value.replace(/\s+/g, ' ').replace(/;/g, '');
+    if (tag.length > 1 && !disability.includes(tag)) {
+      if (disability.length < 20) {
+        tag.split(',').forEach(tag => {
+          disability.push(tag);
+          createDis();
+        });
+      }
     }
+    e.target.value = '';
+  }
 }
 
-tagInputs[2].addEventListener("keyup", addTag);
+tagInputs[2].addEventListener('keyup', disAddTag);
--- models/casefile.py
+++ models/casefile.py
@@ -5,7 +5,7 @@ properties of records in this application.
 """
 
 
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from models.base_record import Record
 from models.base_person import Base
@@ -13,15 +13,18 @@ from models.base_person import Base
 class caseFile(Record, Base):
     """
     This is just the basic information
-    for general purpose
+    for general purpose to be filled by doctors
     """
 
     __tablename__ = "CaseFile"
     symptoms = Column(String(128), nullable=False)
     diagnosis = Column(String(128), nullable=False)
     prescription = Column(String(128), nullable=False)
-    testResult = Column(String(128))
     staff_id = Column(String(60), ForeignKey("doctor.allperson_id"), nullable=False)
+    complaints = Column(Text)
+    examination = Column(Text)
+    observation = Column(Text)
+    impression = Column(Text)
     patient_id = Column(String(60), ForeignKey("patient.allperson_id"), nullable=False)
     doctor, patient = relationship('Doctor'), relationship('Patient')
     healthcare_id = Column(String(60), ForeignKey("institution.id"), nullable=False)
