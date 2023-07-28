1c7957db7c82e463c9db1eba0751bf231c1fa4b8
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-07-28 04:37:03 +0100

    casefile

--- docs/static/scripts/casefile-register.js
+++ docs/static/scripts/casefile-register.js
@@ -1,6 +1,14 @@
 import { allergy, tags, disability } from "./casefile-taginput.js";
 const form = document.querySelector('form');
 const submit = document.querySelector('button.submit-btn');
+const displayName = document.querySelector('h3.patient-name');
+const searchPat = document.getElementById("search-patient");
+const errorDisplay = document.getElementById("error");
+const nin = document.getElementById('nin-variable').dataset.nin;
+
+let allData;
+let staffData;
+
 const formdata = new FormData(form);
 const config = {
     headers: {
@@ -10,6 +18,57 @@ const config = {
   };
 
 // console.log(form, submit, formdata);
+axios.get('http://127.0.0.1:5000/api/v1/dashboarddata/' + nin, {
+  headers: {
+    Authorization: 'Bearer ' + sessionStorage.getItem('healthvaultaccesstoken')
+}
+})
+  .then(res => {
+    staffData = res.data;
+    if (!res.data.institution) {
+      alert('you have not signed into any institution. Please sign in again');
+      window.location.href = '/signin';
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    window.location.href = '/signin';
+  })
+
+searchPat.addEventListener("input", function(event) {
+    let patNin = event.target.value;
+    if (patNin.length === 11) {
+        axios.get(`http://127.0.0.1:5000/api/v1/user_from_nin/${patNin}`, {
+        headers: {
+            Authorization: 'Bearer ' + sessionStorage.getItem('healthvaultaccesstoken')
+        }
+    })
+    .then((response) => {
+        allData = response.data;
+        const patientName = `${allData.patient_data.surname} ${allData.patient_data.firstname} ${allData.patient_data.middlename}`;
+        displayName.innerText = patientName;
+        searchPat.style.display = 'none';
+        displayName.style.display = 'block';
+    })
+    .catch((error) => {
+        console.error(error);
+        errorDisplay.innerText = 'error finding patient\'s data or NIN does not exist. click to try again';
+        searchPat.style.display = 'none';
+        errorDisplay.style.display = 'block';
+        patNin = '';
+    })
+    }
+})
+
+displayName.addEventListener('click', function() {
+    displayName.style.display = 'none';
+    searchPat.style.display = 'block';
+})
+
+errorDisplay.addEventListener('click', () => {
+    errorDisplay.style.display = 'none';
+    searchPat.style.display = 'block';
+})
 
 submit.addEventListener("click", (register) => {
     register.preventDefault();
@@ -18,8 +77,15 @@ submit.addEventListener("click", (register) => {
     formdata.forEach((value, key) => {
         filedata[key] = value;
     })
+    filedata["symptoms"] = JSON.stringify(tags);
+    try {
+        filedata.staff_id = staffData.user.id;
+        filedata.healthcare_id = staffData.institution.id;
+        filedata.patient_id = allData.patient_data.id
+        } catch { err => {console.error(err)}
+    }
 
-    axios.post("http://127.0.0.1:5000/api/v1/casefile", config)
+    axios.post("http://127.0.0.1:5000/api/v1/casefile", filedata, config)
     .then(response => console.log(response.data));
     console.log(filedata);
 })
